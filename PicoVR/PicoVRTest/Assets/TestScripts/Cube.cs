6d3a1f49943da3091cc7a77e989e0820ff8ac346
heqiwu <64227010@qq.com>
2017-07-06 17:54:55 +0800

    Signed-off-by: heqiwu <64227010@qq.com> 2017-7-6第一次提交，推送

---
+++ PicoVR/PicoVRTest/Assets/TestScripts/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+
+//这个是挂在能引起射线反应的物体上，继承自ObjectEventListener（继承自EventTrigger，增加一个绑定方法），通过在Start方法中注册事件触发对应的方法
+public class Cube : ObjectEventListener
+{
+    void Start()
+    {
+        //对应的事件注册对应的方法
+        EventTriggerBind(EventTriggerType.PointerClick, OnPointerClick);
+        EventTriggerBind(EventTriggerType.PointerEnter, OnPointerEnter);
+    }
+    public void OnPointerClick(BaseEventData eventData)
+    {
+        //base.OnPointerClick(arg0);
+        print("点击了，这是派生类中发出的消息");
+    }
+    public void OnPointerEnter(BaseEventData eventData)
+    {
+        print("移入了，这是覆盖方法中发出的消息");
+    }
+}
---
+++ PicoVR/PicoVRTest/Assets/TestScripts/TestTextClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTextClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTextClick()
+    {
+        print("点击了TEXT");
+    }
+}
