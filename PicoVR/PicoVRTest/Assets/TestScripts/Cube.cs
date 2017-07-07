8d3735cdbc41a730e1529c5f64b6f6695371e75c
heqiwu <64227010@qq.com>
2017-07-07 17:49:13 +0800

    Signed-off-by: heqiwu <64227010@qq.com> 为地图作准备，先用养鸡场试试，第一次提交

--- PicoVR/PicoVRTest/Assets/TestScripts/Cube.cs
+++ PicoVR/PicoVRTest/Assets/TestScripts/Cube.cs
@@ -9,16 +9,35 @@ public class Cube : ObjectEventListener
     void Start()
     {
         //对应的事件注册对应的方法
-        EventTriggerBind(EventTriggerType.PointerClick, OnPointerClick);
-        EventTriggerBind(EventTriggerType.PointerEnter, OnPointerEnter);
+        //EventTriggerBind(EventTriggerType.PointerClick, OnPointerClick);
+        //EventTriggerBind(EventTriggerType.PointerEnter, OnPointerEnter);
+
+        Pvr_ControllerRayCheck.RayCheckEvent += check;
+        Pvr_ControllerRayCheck.RayPressEvent += press;
     }
-    public void OnPointerClick(BaseEventData eventData)
+    //public void OnPointerClick(BaseEventData eventData)
+    //{
+    //    //base.OnPointerClick(arg0);
+    //    print("点击了，这是派生类中发出的消息");
+    //}
+    //public void OnPointerEnter(BaseEventData eventData)
+    //{
+    //    print("移入了，这是覆盖方法中发出的消息");
+    //}
+    void check(object sender, EventArgs e)
     {
-        //base.OnPointerClick(arg0);
-        print("点击了，这是派生类中发出的消息");
+        RayCheckArgs arg = (RayCheckArgs)e;
+        if(arg.checkObject == this.gameObject)
+        {
+            print("检测到了CUBE");
+        }
     }
-    public void OnPointerEnter(BaseEventData eventData)
+    void press(object sender, EventArgs e)
     {
-        print("移入了，这是覆盖方法中发出的消息");
+        RayCheckArgs arg = (RayCheckArgs)e;
+        if(arg.checkObject == this.gameObject)
+        {
+            print("按下了CUBE");
+        }
     }
 }
---
+++ PicoVR/YangJiChang/Assets/MoveController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+    public static bool isForward;
+    public static bool isBack;
+    public static bool isLeft;
+    public static bool isRight;
+
+    public float speed;
+    public float rotationSpeed;
+
+    CharacterController controller;
+    Transform camera;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        camera = transform.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Direction();
+        Rotation();
+	}
+    void Direction()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            isForward = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            isBack = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            isLeft = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            isRight = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isForward = false;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            isBack = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            isLeft = false;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            isRight = false;
+        }
+    }
+    void Rotation()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+        if(x != 0)
+        {
+            transform.Rotate(transform.up, x * rotationSpeed, Space.World);
+        }
+        if(y != 0)
+        {
+            camera.Rotate(camera.right, y * rotationSpeed, Space.World);
+        }
+    }
+    void LateUpdate()
+    {
+        if(isForward)
+        {
+            controller.SimpleMove(transform.forward * Time.deltaTime * speed);
+        }
+        else if(isBack)
+        {
+            controller.SimpleMove(-transform.forward * Time.deltaTime * speed);
+        }
+        if(isLeft)
+        {
+            controller.SimpleMove(-transform.right * Time.deltaTime * speed);
+        }
+        else if(isRight)
+        {
+            controller.SimpleMove(transform.right * Time.deltaTime * speed);
+        }
+    }
+}
