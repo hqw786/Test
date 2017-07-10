05efc247281acff90638cebb7ea176fed811f76a
heqiwu <64227010@qq.com>
2017-07-10 23:58:15 +0800

    Signed-off-by: heqiwu <64227010@qq.com> 回来是做了一点点。不知道怎么的没心情做。真烦

--- PicoVR/YangJiChang/Assets/MainManager.cs
+++ PicoVR/YangJiChang/Assets/MainManager.cs
@@ -6,20 +6,59 @@ using UnityStandardAssets.Characters.FirstPerson;
 public enum ViewMode
 {
     firstView,
-    thirdView,
+    //thirdView,
     flyView
 }
 
 public class MainManager : MonoBehaviour {
+	Transform person;
+
     FirstPersonController firstPerson;
-    
+	FlyController flyController;
+
+	ViewMode view;
 	// Use this for initialization
+	void Awake()
+	{
+		person = transform.Find("/FPSController");
+		firstPerson = person.GetComponent<FirstPersonController>();
+		flyController = person.GetComponent<FlyController>();
+		view = ViewMode.firstView;
+	}
 	void Start () {
-		
+		flyController.enabled = false;
+		firstPerson.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
+	}
+	void ModeSwitch()
+	{
+		view = view == ViewMode.firstView ? ViewMode.flyView : ViewMode.firstView;
+		if(view == ViewMode.firstView)
+		{
+			firstPerson.enabled = true;
+			flyController.enabled = false;
+			positionSwitch(view);
+		}
+		else
+		{
+			firstPerson.enabled = false;
+			flyController.enabled = true;
+			positionSwitch(view);
+		}
+	}
+	void positionSwitch(ViewMode view)
+	{
+		if(view == ViewMode.firstView)
+		{
+			person.position = new Vector3(person.position.x, 50f, person.position.z);
+		}
+		else
+		{
+			person.position = new Vector3(person.position.x, 0f, person.position.z);
+		}
 	}
 }
