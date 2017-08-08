fd28e08959d30ae1839435113becf310d496330d
hqw786 <64227010@qq.com>
2017-08-08 23:39:06 +0800

    Signed-off-by: hqw786 <64227010@qq.com> 增加一个MVC的DEMO

---
+++ PicoVR/MVCDemo/Assets/View/PackItem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PackItem : MonoBehaviour
+{
+
+    Text text;
+
+    Image img;
+
+    void Awake()
+    {
+        this.text = this.transform.Find("Text").GetComponent<Text>();
+        this.img = this.transform.Find("GoodImg").GetComponent<Image>();
+    }
+
+    private PackModel model;
+
+    public PackModel Model
+    {
+        get { return model; }
+        set
+        {
+            model = value;
+            if (model.GoodId != 0)
+            {
+                this.img.enabled = true;
+                this.text.text = model.Count.ToString();
+                this.img.sprite = Resources.Load<Sprite>(model.good.Src);
+               
+            }
+            else
+            {
+                this.img.enabled = false;
+                this.text.text = "0";
+                this.img.sprite = null;
+            }
+	      
+           
+        }
+    }
+
+
+
+}
