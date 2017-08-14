f4bb56c6568b4c5028797de0089b1a5895d2e43d
hqw786 <64227010@qq.com>
2017-08-14 19:45:58 +0800

    Signed-off-by: hqw786 <64227010@qq.com> MVC_ShiYanShi

---
+++ PicoVR/MVC_ShiYanShi/Assets/APP/Scripts/BaseMVC/BaseModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseModel<T>
+{
+    protected int ID { get; set; }
+    protected string Name { get; set; }
+    protected string Des { get; set; }
+
+    protected BaseModel(int id)
+    {
+        ID = id;
+    }
+    protected BaseModel(int id, string name):this(id)
+    {
+        Name = name;
+    }
+    protected BaseModel(int id, string name, string des):this(id,name)
+    {
+        Des = des;
+    }
+    public abstract T GetModel();
+}
