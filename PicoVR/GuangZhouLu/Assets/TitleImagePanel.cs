058b6e7817857e8907948bd21bc922acb16f56d8
heqiwu <64227010@qq.com>
2017-08-15 18:08:21 +0800

    Signed-off-by: heqiwu <64227010@qq.com> 更换随缘大厦模型，设计院LOGO修改白底黑字

--- PicoVR/GuangZhouLu/Assets/TitleImagePanel.cs
+++ PicoVR/GuangZhouLu/Assets/TitleImagePanel.cs
@@ -7,19 +7,25 @@ public class TitleImagePanel : MonoBehaviour {
     float time = 1.5f;
     Transform textTransform;
     UIImageEffect uiiePanel;
+    UITextEffect uitePanel;
     //UITextEffect uiteText;
 	// Use this for initialization
 	void Start () {
         textTransform = transform.Find("Image");
 
         uiiePanel = textTransform.GetComponent<UIImageEffect>();
+        uitePanel = transform.Find("Text").GetComponent<UITextEffect>();
+
         uiiePanel.SetAlphaOneWay(0f, 1f, time);
+        uitePanel.SetAlphaOneWay(0f, 1f, time);
+
         Invoke("Hide", 4f);
         Invoke("LoadScene", 6f);
 	}
 	void Hide()
     {
         uiiePanel.SetAlphaOneWay(1f, 0f, time);
+        uitePanel.SetAlphaOneWay(1f, 0f, time);
     }
 	// Update is called once per frame
 	void Update () {
