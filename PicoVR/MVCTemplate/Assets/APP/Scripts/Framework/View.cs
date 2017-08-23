89a744c079200a47acaf9d80872b690e2a91a742
heqiwu <64227010@qq.com>
2017-08-23 14:57:06 +0800

    Signed-off-by: heqiwu <64227010@qq.com> MVC模板完成

---
+++ PicoVR/MVCTemplate/Assets/APP/Scripts/Framework/View.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class View : MonoBehaviour 
+{
+    public abstract string Name { get; }
+    
+    public List<string> attentionEvents = new List<string>();
+    
+    public virtual void RegisterEvents()
+    {
+        
+    }
+    protected T GetModel<T>() where T : Model
+    {
+        return MVC.GetModel<T>() as T;
+    }
+    protected T GetView<T>() where T : View
+    {
+        return MVC.GetView<T>() as T;
+    }
+    public abstract void HandleEvent(string eventName, object data);
+    protected void SendEvent(string eventName, object data = null)
+    {
+        MVC.SendEvent(eventName, data);
+    }
+}
