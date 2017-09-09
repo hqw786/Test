bd27333941ee0855fa12e81881fb3a0eb0beaf92
hqw786 <64227010@qq.com>
2017-09-09 12:26:54 +0800

    Signed-off-by: hqw786 <64227010@qq.com> 增加养鸡场自动化

---
+++ PicoVR/yangjichang/Assets/EggLaying.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggLaying : MonoBehaviour {
+    public bool isLeftDir;
+    public bool isRightDir;
+
+    float RandLayEggTime;
+    float timer;
+    bool isLayEgg;
+
+    GameObject eggPrefab;
+    Transform parent;
+	// Use this for initialization
+    void Awake()
+    {
+        isLayEgg = false;
+        eggPrefab = Resources.Load<GameObject>("Prefabs/jidan");
+        parent = GameObject.Find("/Eggs").transform;
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLayEgg)
+        {
+            isLayEgg = false;
+            RandLayEggTime = Random.Range(10, 21);
+            //多少概率产蛋
+            if(Random.Range(1,101) == 1)
+            {
+                //实例化鸡蛋
+                GameObject egg = Instantiate(eggPrefab);
+                egg.transform.parent = parent;
+                egg.transform.localScale = Vector3.one;
+                if(isLeftDir)
+                {
+                    egg.transform.position = transform.position + new Vector3(0f,0f,-0.1f);
+                }
+                if(isRightDir)
+                {
+                    egg.transform.position = transform.position + new Vector3(0f, 0f, 0.1f);
+                }
+            }
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if(timer >= RandLayEggTime)
+            {
+                timer = 0f;
+                isLayEgg = true;
+            }
+        }
+	}
+}
---
+++ PicoVR/yangjichang/Assets/RandPlayAnimator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandPlayAnimator : MonoBehaviour {
+    Animator anim;
+    float randTime;
+    float timer;
+    int randNum;
+    bool isIdle;
+	// Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        isIdle = true;
+    }
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isIdle)
+        {
+            isIdle = false;
+            randNum = Random.Range(2, 7);
+            anim.SetInteger("Action", randNum);
+            randTime = Random.Range(5f, 10f);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if(timer >= randTime)
+            {
+                isIdle = true;
+                timer = 0f;
+            }
+            anim.SetInteger("Action", 0);
+            //anim.runtimeAnimatorController
+        }
+	}
+}
